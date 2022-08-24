caa4fc5d7d2080fc0d228485d053838d13ac26d1
Bohdana_Vashchuk <bogdana.vashchuk@gmail.com>
2022-08-24 18:06:55 +0300

    Commit Task4

---
+++ src/main/java/Task4/Task4.java
@@ -0,0 +1,26 @@
+package Task4;
+
+import Task3.ReadJavaFile;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+
+public class Task4 {
+    public static final Logger LOGGER = LogManager.getLogger(Task4.class);
+    public static void main(String[] args) {
+        contentOfDirectory();
+    }
+    public static void contentOfDirectory(){
+        File file = new File("C:\\Users\\Bohdana_Vashchuk\\Desktop\\Project");
+        File[] files = file.listFiles();
+        for (File object: files) {
+            if(object.isFile()){
+                LOGGER.info("File name: " + object.getName());
+            }
+            else if(object.isDirectory()){
+                LOGGER.info("Directory name is: " + object.getName());
+            }
+        }
+    }
+}
